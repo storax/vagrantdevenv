b91fec4a85c2b9fae7819a1099e0f89ddfb87766
David Zuber <zuber.david@gmx.de>
2015-02-03 20:15:18 +0100

    Install git, emacs, zsh, guake, homesick and symlink dotfiles.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,11 @@
 sudo apt-get update
+sudo apt-get upgrade -y
+
 sudo apt-get install ubuntu-desktop -y
+sudo apt-get install git -y
+sudo apt-get install emacs -y
+sudo apt-get install zsh -y
+sudo apt-get install guake -y
+sudo gem install homesick
+homesick clone storax/storaxcastle
+homesick symlink storaxcastle
