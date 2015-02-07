a5fd15a366eb0bc4b581056b1580d6e4baa378a4
David Zuber <zuber.david@gmx.de>
2015-02-07 23:56:01 +0000

    preinstall some packages for the desktop, then clean up

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,27 +3,16 @@ sudo apt-get upgrade -y
 sudo apt-get install ubuntu-desktop -y --no-install-recommends
 sudo apt-get upgrade -y
 
-
 declare -a pkgs=(
-    app-install-data-partner
     emacs
-    firefox
-    gcc
     git
-    gitk
-    gnome-disk-utility
     gnome-terminal
-    guake
     indicator-application
     indicator-appmenu
     indicator-messages
     indicator-renderer
     indicator-session
     make
-    network-manager-gnome
-    overlay-scrollbar
-    python-pip
-    ubuntu-system-settings
     unity-lens-applications
     unity-lens-files
     zsh
@@ -36,16 +25,7 @@ do
    echo Finished installing $i
 done
 
-
-sudo gem install homesick
-homesick clone storax/storaxcastle
-homesick symlink storaxcastle
-
-sudo pip install virtualenvwrapper
-
-echo Changing homeshell to zsh
-sudo chsh -s $(which zsh) vagrant
-
-echo Start downloading antigen
-curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/antigen.zsh
-echo Downloading antigen finished!
+sudo apt-get clean
+sudo dd if=/dev/zero of=/EMPTY bs=1M
+sudo rm -f /EMPTY
+cat /dev/null > ~/.bash_history && history -c && exit
