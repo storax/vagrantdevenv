538379ba35e298d7782114580f41115a42867425
David Zuber <zuber.david@gmx.de>
2015-02-08 12:02:39 +0000

    Use preconfigured box

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,41 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install ubuntu-desktop -y --no-install-recommends
-sudo apt-get upgrade -y
-
-
-declare -a pkgs=(
-    app-install-data-partner
-    emacs
-    firefox
-    gcc
-    git
-    gitk
-    gnome-disk-utility
-    gnome-terminal
-    guake
-    indicator-application
-    indicator-appmenu
-    indicator-messages
-    indicator-renderer
-    indicator-session
-    make
-    network-manager-gnome
-    overlay-scrollbar
-    python-pip
-    ubuntu-system-settings
-    unity-lens-applications
-    unity-lens-files
-    zsh
-)
-
-for i in "${pkgs[@]}"
-do
-   echo Installing $i
-   sudo apt-get install -y $i
-   echo Finished installing $i
-done
-
 
 sudo gem install homesick
 homesick clone storax/storaxcastle
