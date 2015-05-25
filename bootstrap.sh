8adda7a99b645eb72fa8c4d4244315fc21e59267
David Zuber <zuber.david@gmx.de>
2015-05-25 20:57:49 +0200

    use zsh for cloning github repos

--- bootstrap.sh
+++ bootstrap.sh
@@ -66,7 +66,7 @@ echo Installing Pyside Phonon VLC Backend finished!
 mkdir ~/projects
 git clone https://gist.github.com/6402dc6c4759ebd8123e.git ~/projects/clonegithubrepos
 cd ~/projects
-bash ~/projects/clonegithubrepos/clonegithubrepos.sh storax
+zsh ~/projects/clonegithubrepos/clonegithubrepos.sh storax
 find . -wholename '*/.git/config' -exec sed -i 's#https://github.com/#git@github.com:#g' {} +
 cd ~
 
