437ff9c3817c1bc135f899f56bcd399f542821a2
David Zuber <zuber.david@gmx.de>
2015-05-27 17:54:06 +0200

    configure guake appearance and behavior

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,7 @@ declare -a pkgs=(
    firefox
    fonts-dejavu
    fonts-inconsolata
+   gconf2
    git
    git-flow
    git-flow
@@ -135,3 +136,7 @@ sudo gem install travis
 echo finish installing travis
 
 gsettings set com.canonical.Unity.Launcher favorites "['application://org.gnome.Nautius.desktop','application://firefox.desktop','application://unity-control-center.desktop','application://emacs.desktop','appliaction://pidgin.desktop','unity://running-apps','unity://expo-icon','unity://devices']"
+gconftool-2 --set "/app/guake/style/background/transparency" --type=int 15
+gconftool-2 --set "/app/guake/general/window_tabbar" --type=bool true
+gconftool-2 --set "/app/guake/general/window_height" --type=int 75
+
