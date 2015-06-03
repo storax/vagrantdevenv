49b02c3527820497939cdd3fade994a901ef0bbc
David Zuber <zuber.david@gmx.de>
2015-06-03 15:07:23 +0000

    Install elpy venvwrapper pyflakes in all pyenvs

--- pyenv.sh
+++ pyenv.sh
@@ -6,6 +6,8 @@ echo Installation of pyenv finished!
 
 git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
 
+pip install elpy virtualenvwrapper pyflakes
+
 echo Creating python envs
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
@@ -14,6 +16,7 @@ pyenv install 2.7.9
 pyenv install 3.4.3
 pyenv rehash
 pyenv global 3.4.3
+pyenv shell 2.7.9
+pip install elpy virtualenvwrapper pyflakes
 pyenv shell 3.4.3
-
 pip install elpy virtualenvwrapper pyflakes
